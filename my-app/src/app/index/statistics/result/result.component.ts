19541ca3f4368752212f2b433d730aa551114490
Unknown <savozepinic@gmail.com>
2019-03-09 19:36:10 +0100

    result last round

--- my-app/src/app/index/statistics/result/result.component.ts
+++ my-app/src/app/index/statistics/result/result.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { AgGridNg2 } from 'ag-grid-angular';
 import { HttpClient } from '@angular/common/http';
-import { API_ENDPOINT } from '../../../_constants/url.constants';
+import { API_ENDPOINT } from 'src/app/_constants/url.constants';
+import { AgGridNg2 } from 'ag-grid-angular';
 
 @Component({
-  selector: 'app-statistics-index',
-  templateUrl: './statistics-index.component.html',
-  styleUrls: ['./statistics-index.component.css']
+  selector: 'app-result',
+  templateUrl: './result.component.html',
+  styleUrls: ['./result.component.css']
 })
-export class StatisticsIndexComponent implements OnInit {
+export class ResultComponent implements OnInit {
+
   @ViewChild('agGrid') agGrid: AgGridNg2;
   matches;
   columnDefs = [
@@ -16,24 +17,30 @@ export class StatisticsIndexComponent implements OnInit {
     { headerName: 'Liga', field: 'leagueName', width: 120 },
     { headerName: 'Domaćin', field: 'homeName', width: 120 },
     { headerName: 'Avrg', field: 'homeAvrg', width: 70 },
-    { headerName: 'H.G.', field: 'homeGoals', width: 70 },
     { headerName: '3+ %', field: 'homeThreePlus', width: 70 },
     { headerName: '3+ niz', field: 'homeThreePlusInRow', width: 80 },
-    { headerName: 'Last 3', field: 'homeLastThreeArg', width: 80 },
     { headerName: 'Gost', field: 'awayName', width: 120 },
     { headerName: 'Avrg', field: 'awayAvrg', width: 70 },
-    { headerName: 'A.G.', field: 'awayGoals', width: 70 },
     { headerName: '3+', field: 'awayThreePlus', width: 70 },
     { headerName: '3+ niz', field: 'awayThreePlusInRow', width: 80 },
-    { headerName: 'Last 3', field: 'awayLastThreeArg', width: 80 },
     { headerName: 'Avrg', field: 'avrg', width: 90 },
-    { headerName: 'Ukupno', field: 'total', width: 90 }
+    { headerName: 'Ukupno', field: 'total', width: 90 },
+    {
+      headerName: 'Goals', field: 'goals', width: 90, cellStyle: function (params) {
+        console.log(params);
+        if (params.data.isGood) {
+          return { color: 'white', backgroundColor: 'green' };
+        } else {
+          return { color: 'white', backgroundColor: 'red' };
+        }
+      }
+    }
   ];
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.matches = this.http.get(API_ENDPOINT + 'statistics/getstatistics');
+    this.matches = this.http.get(API_ENDPOINT + 'statistics/GetLastRoundStatistics');
 
   }
 
